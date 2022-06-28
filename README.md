b68887b556a42d9ad8099707ea00f1fe8da39f28
MarkBrooks3 <brooks3mark@gmail.com>
2022-06-28 01:19:06 -0500

    added a display for the values

--- README.md
+++ README.md
@@ -3,5 +3,4 @@ A user may change the values of the border-radius on the shape and view the resu
 
 TODO:
 -general styling and positioning
--display the values of border-radius
 -allow the user to copy the current border-radius settings
--- index.js
+++ index.js
@@ -3,41 +3,53 @@ const rightSlider = document.getElementById('right');
 const topSlider = document.getElementById('top');
 const bottomSlider = document.getElementById('bottom');
 const shape = document.getElementById('shape');
+const display = document.getElementById('display');
 
-shape.style.borderRadius = `${topSlider.value}% ${100 - topSlider.value}% ${
+let values = `${topSlider.value}% ${100 - topSlider.value}% ${
   100 - bottomSlider.value
 }% ${bottomSlider.value}% / ${leftSlider.value}% ${rightSlider.value}% ${
   100 - rightSlider.value
 }% ${100 - leftSlider.value}%`;
 
+shape.style.borderRadius = values;
+display.innerHTML = values;
+
 leftSlider.oninput = () => {
-  shape.style.borderRadius = `${topSlider.value}% ${100 - topSlider.value}% ${
+  values = `${topSlider.value}% ${100 - topSlider.value}% ${
     100 - bottomSlider.value
   }% ${bottomSlider.value}% / ${leftSlider.value}% ${rightSlider.value}% ${
     100 - rightSlider.value
   }% ${100 - leftSlider.value}%`;
+  shape.style.borderRadius = values;
+  display.innerHTML = values;
 };
 
 bottomSlider.oninput = () => {
-  shape.style.borderRadius = `${topSlider.value}% ${100 - topSlider.value}% ${
+  values = `${topSlider.value}% ${100 - topSlider.value}% ${
     100 - bottomSlider.value
   }% ${bottomSlider.value}% / ${leftSlider.value}% ${rightSlider.value}% ${
     100 - rightSlider.value
   }% ${100 - leftSlider.value}%`;
+  shape.style.borderRadius = values;
+  display.innerHTML = values;
 };
 
 rightSlider.oninput = () => {
-  shape.style.borderRadius = `${topSlider.value}% ${100 - topSlider.value}% ${
+  values = `${topSlider.value}% ${100 - topSlider.value}% ${
     100 - bottomSlider.value
   }% ${bottomSlider.value}% / ${leftSlider.value}% ${rightSlider.value}% ${
     100 - rightSlider.value
   }% ${100 - leftSlider.value}%`;
+  shape.style.borderRadius = values;
+  display.innerHTML = values;
 };
 
 topSlider.oninput = () => {
-  shape.style.borderRadius = `${topSlider.value}% ${100 - topSlider.value}% ${
+  values = `${topSlider.value}% ${100 - topSlider.value}% ${
     100 - bottomSlider.value
   }% ${bottomSlider.value}% / ${leftSlider.value}% ${rightSlider.value}% ${
     100 - rightSlider.value
   }% ${100 - leftSlider.value}%`;
+  shape.style.borderRadius = values;
+  display.innerHTML = values;
 };
